bd8bae3f236a2b3c7a0791e3f4f72a06c93002df
kkn <kr676628@dal.ca>
2021-12-06 21:28:07 -0400

    Added location service and InitController

--- src/main/java/com/dalhousie/bloodDonation/constants/BloodDonationConstants.java
+++ src/main/java/com/dalhousie/bloodDonation/constants/BloodDonationConstants.java
@@ -3,4 +3,5 @@ package com.dalhousie.bloodDonation.constants;
 public class BloodDonationConstants {
     public final static String APP_NAME = "Blood Donation System";
     public final static long OTPTIMEOUT = 1l*60l;
+    public final static String DIRECTION_SYM = "->";
 }
---
+++ src/test/java/com/dalhousie/bloodDonation/service/LocationServiceTest.java
@@ -0,0 +1,15 @@
+package com.dalhousie.bloodDonation.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LocationServiceTest {
+
+    @Test
+    void getShortestPath() {
+    }
+
+    @Test
+    void getDistanceInKms() {
+    }
+}
