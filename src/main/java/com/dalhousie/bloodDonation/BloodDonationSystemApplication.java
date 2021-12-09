37edc096f0cea8782ebb1f6dcc3cf5ae3e5b1f2e
kkn <kr676628@dal.ca>
2021-12-09 06:33:21 -0400

    Redirected the start to Init

--- src/main/java/com/dalhousie/bloodDonation/BloodDonationSystemApplication.java
+++ src/main/java/com/dalhousie/bloodDonation/BloodDonationSystemApplication.java
@@ -1,51 +1,16 @@
 package com.dalhousie.bloodDonation;
 
-import com.dalhousie.bloodDonation.controller.FinancialDonationController;
-import com.dalhousie.bloodDonation.controller.AppointmentController;
-import com.dalhousie.bloodDonation.controller.DonorAppointmentController;
-import com.dalhousie.bloodDonation.controller.DonorMedicalRecordController;
-import com.dalhousie.bloodDonation.controller.ListSuitableDonorController;
-import com.dalhousie.bloodDonation.utils.DBUtils;
-import com.dalhousie.bloodDonation.controller.MenuController;
-import com.dalhousie.bloodDonation.exception.CustomException;
-
+import com.dalhousie.bloodDonation.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.text.ParseException;
 
 @SpringBootApplication
 public class BloodDonationSystemApplication {
 
-    public static void main(String[] args) throws SQLException, ParseException, CustomException {
-        DBUtils dbUtils = new DBUtils();
-        Connection conn = dbUtils.getConnection();
-        
-        // AppointmentController appointmentController= new AppointmentController();
-        // DonorMedicalRecordController donorMedicalRecordController = new DonorMedicalRecordController();
-        DonorAppointmentController donorAppointmentController = new DonorAppointmentController();
-        // ListSuitableDonorController listSuitableDonorController= new ListSuitableDonorController();
-        // appointmentController.displayAppointmentTime();
-        // appointmentController.bookDate();
-        // appointmentController.ShowPersonInactiveList();
-        // appointmentController.MakePersonActive();
-        // donorMedicalRecordController.addMedicalRecords();
-        // donorMedicalRecordController.editMedicalRecords();
-        // donorAppointmentController.seeDonorRequests();
-        // donorAppointmentController.todayDonationConfirmation();
-        // donorAppointmentController.confirmDonorRequests();
-        // donorAppointmentController.displayAppointmentTime();
-        // donorAppointmentController.bookDate();
-        // listSuitableDonorController.patientDonorList();
-        // listSuitableDonorController.organisationDonorSelection();
-        // listSuitableDonorController.organisationDonorSelection();
-        // MenuController menuController = new MenuController();
-        // donorAppointmentController.seePatientRequestStatus();
-        donorAppointmentController.todayPatientRequestConfirmation();
-        // menuController.displayMenu();
+    public static void main(String[] args) {
         SpringApplication.run(BloodDonationSystemApplication.class, args);
+        new InitController().mainMenu();
+        new OrgBloodRequestController().mainMenu();
     }
 
 }
