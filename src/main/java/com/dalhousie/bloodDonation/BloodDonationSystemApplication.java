f5ac390ade9b1c37a608a02b63583b4de58c1e4c
kkn <kr676628@dal.ca>
2021-12-09 06:34:23 -0400

    Main issue fix

--- src/main/java/com/dalhousie/bloodDonation/BloodDonationSystemApplication.java
+++ src/main/java/com/dalhousie/bloodDonation/BloodDonationSystemApplication.java
@@ -10,7 +10,6 @@ public class BloodDonationSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(BloodDonationSystemApplication.class, args);
         new InitController().mainMenu();
-        new OrgBloodRequestController().mainMenu();
     }
 
 }
