4c91811b8eafd908b1ea202c7347995cfe117968
Janhavi Sonawane <jn476959@dal.ca>
2021-12-09 11:23:19 -0400

    Added pincode and email

--- src/main/java/com/dalhousie/bloodDonation/repos/LoginRepository.java
+++ src/main/java/com/dalhousie/bloodDonation/repos/LoginRepository.java
@@ -128,10 +128,10 @@ public class LoginRepository {
             UUID uuid = UUID.randomUUID();
             String uuidAsString = uuid.toString();
             Statement st = con.createStatement();
-            st.execute("INSERT INTO organisation(organisation_id,organisation_name,location,organisation_type,password,slots_available,pin_code,email) " +
+            st.execute("INSERT INTO organisation(organisation_id,organisation_name,location,organisation_type,password,slots_available) " +
                     "VALUES('"+ uuidAsString +"','"+ organisation.getorganisationName()+"','"+ organisation.getLocation()+"','"+ organisation
-                    .getorganisationType()+"','"+ organisation.getPassword()+"','"+ organisation.getSlots_available()+"','"+ organisation.getPinCode()+"','"+ organisation.getEmail()+"')");
-            st.execute("Insert into user(username,password,firstname,userId,userType) values ('"+organisation.getEmail()+"','"+organisation.getPassword()+"','"+organisation.getorganisationName()+"','"+uuidAsString+"','"+UserType.ORGANIZATION+"')");
+                    .getorganisationType()+"','"+ organisation.getPassword()+"','"+ organisation.getSlots_available()+"')");
+            st.execute("Insert into user(username,password,firstname,userId,userType) values ('"+organisation.getorganisationName()+"','"+organisation.getPassword()+"','"+organisation.getorganisationName()+"','"+uuidAsString+"','"+UserType.ORGANIZATION+"')");
             System.out.println("SignUp successful");
         }catch (SQLException e) {
             e.printStackTrace();
@@ -191,7 +191,7 @@ public class LoginRepository {
             UUID uuid = UUID.randomUUID();
             String uuidAsString = uuid.toString();
             Statement st = con.createStatement();
-            st.execute("INSERT INTO Person(person_id,person_first_name,person_last_name,contact_number,blood_group,pin_code) VALUES('"+ uuidAsString+"','"+ user.getFirstname()+"','"+ user.getLastname()+"','"+ contactNo+"','"+ user.getBloodGroup()+"','"+ pinCode +"')");
+            st.execute("INSERT INTO Person(person_id,person_first_name,person_last_name,contact_number,blood_group) VALUES('"+ uuidAsString+"','"+ user.getFirstname()+"','"+ user.getLastname()+"','"+ contactNo+"','"+ user.getBloodGroup()+"')");
             st.execute("Insert into user (userId,username,password,firstname,lastname,bloodgroup,userType) values('"+uuidAsString+"','"+user.getUserName()+"','"+user.getPassword()+"','"+user.getFirstname()+"','"+user.getLastname()+"','"+user.getBloodGroup()+"','"+ UserType.DONOR.toString() +"')");
             System.out.println("SignUp successfull");
         }catch (SQLException e) {
