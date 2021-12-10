49c7e9c6eaf893d7cf2ad2936d3c1fa72d8b795a
kkn <kr676628@dal.ca>
2021-12-10 00:00:20 -0400

    Added Test cases

--- src/test/java/com/dalhousie/bloodDonation/service/OrgBloodDonationServiceTest.java
+++ src/test/java/com/dalhousie/bloodDonation/service/OrgBloodDonationServiceTest.java
@@ -1,33 +1,49 @@
 package com.dalhousie.bloodDonation.service;
 
+import com.dalhousie.bloodDonation.constants.BloodGroup;
 import com.dalhousie.bloodDonation.exception.CustomException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrgBloodDonationServiceTest {
 
+    private static OrgBloodDonationService orgBloodDonationService;
+    @BeforeAll
+    static void setUp(){
+        orgBloodDonationService = new OrgBloodDonationServiceImpl();
+    }
+
     @Test
     void getListByDonorId() throws CustomException {
-        OrgBloodDonationService orgBloodDonationService = new OrgBloodDonationServiceImpl();
-        orgBloodDonationService.getListByDonorId("O001").forEach(strings -> {
-            for(int i=0; i<strings.length;i++){
-                System.out.print(strings[i]);
-                System.out.print("||");
-            }
-            System.out.println();
-        });
+        Assertions.assertNotNull(orgBloodDonationService.getListByDonorId("5c256da3-3d82-11ec-917b-e2ed2ce588f5"));
     }
 
     @Test
     void getListByBloodGroup() throws CustomException {
-        OrgBloodDonationService orgBloodDonationService = new OrgBloodDonationServiceImpl();
-        orgBloodDonationService.getListByBloodGroup("O001").forEach(strings -> {
-            for(int i=0; i<strings.length;i++){
-                System.out.print(strings[i]);
-                System.out.print("||");
-            }
-            System.out.println();
-        });
+        Assertions.assertNotNull(orgBloodDonationService.getListByBloodGroup("494b72c5-5ffb-46e7-abb5-507f84a5fce6"));
     }
+
+    @Test
+    void testGetListByDonorId() throws CustomException {
+        Assertions.assertNotNull(orgBloodDonationService.getListByDonorId("494b72c5-5ffb-46e7-abb5-507f84a5fce6"));
+    }
+
+    @Test
+    void testGetListByBloodGroup() throws CustomException {
+        Assertions.assertNotNull(orgBloodDonationService.getListByBloodGroup("494b72c5-5ffb-46e7-abb5-507f84a5fce6"));
+    }
+
+    @Test
+    void getPendingRequests() throws CustomException {
+        Assertions.assertNotNull(orgBloodDonationService.getPendingRequests("494b72c5-5ffb-46e7-abb5-507f84a5fce6"));
+    }
+
+    @Test
+    void getRecommendedOrganisation() throws CustomException {
+        Assertions.assertNotNull(orgBloodDonationService.getRecommendedOrganisation(2, BloodGroup.ABNeg));
+    }
+
 }
