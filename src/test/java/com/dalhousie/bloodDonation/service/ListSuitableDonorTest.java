0bbf318f354b5fb38092dd2dd39c4515ed757e5f
Amandeep Singh Matta <am759831@dal.ca>
2021-12-09 21:16:37 -0400

    Test

--- src/test/java/com/dalhousie/bloodDonation/service/ListSuitableDonorTest.java
+++ src/test/java/com/dalhousie/bloodDonation/service/ListSuitableDonorTest.java
@@ -1,22 +1,13 @@
 package com.dalhousie.bloodDonation.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
-import java.sql.SQLException;
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import com.dalhousie.bloodDonation.exception.CustomException;
-import com.dalhousie.bloodDonation.model.BloodDonationDetaisHistory;
-import com.dalhousie.bloodDonation.model.DonorMedicalRecords;
-import com.dalhousie.bloodDonation.model.PatientBloodRequest;
+
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
