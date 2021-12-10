750ed7ce8725f5959d7b1be9995343688a31f2ba
Amandeep Singh Matta <am759831@dal.ca>
2021-12-09 21:15:01 -0400

    List suitable Test final

--- src/test/java/com/dalhousie/bloodDonation/service/ListSuitableDonorTest.java
+++ src/test/java/com/dalhousie/bloodDonation/service/ListSuitableDonorTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import com.dalhousie.bloodDonation.exception.CustomException;
 import com.dalhousie.bloodDonation.model.BloodDonationDetaisHistory;
+import com.dalhousie.bloodDonation.model.DonorMedicalRecords;
 import com.dalhousie.bloodDonation.model.PatientBloodRequest;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -28,6 +29,64 @@ class ListSuitableDonorTest {
     @BeforeEach
     void setUp() throws Exception {
         listSuitableDonor= new ListSuitableDonorImpl();
+    }
+
+    @Test
+    @DisplayName("Not null test for  Manage Appointment Class")
+    void listSuitableDonorNotNull() {
+       
+        assertNotNull(listSuitableDonor, "Manage appointment class exist");
+    }
+
+    @Test
+    @DisplayName("Test suitable donor ")
+    void testSuitableDonorId() throws CustomException{
+        List<String> donorid= new ArrayList<String>();
+        donorid.add("5d9b8b40-5213-11ec-917b-e2ed2ce588f5");
+        donorid.add("bce4f555-522c-11ec-917b-e2ed2ce588f5");
+        String bloodType="ANeg";
+        assertEquals(donorid,listSuitableDonor.getSuitableDonorID(bloodType));
+    }
+
+    @Test
+    @DisplayName("Test donor id")
+    void testgetSuitableDonor() throws CustomException {
+        String id="5d9b8b40-5213-11ec-917b-e2ed2ce588f5";
+       
+        String getDonorid=listSuitableDonor.getDonorDetails(id);
+        assertEquals(getDonorid,"Janhavi");
+    }
+
+    @Test
+    void testBloodGroup(){
+        String id="5d9b8b40-5213-11ec-917b-e2ed2ce588f5";
+        String bloodType="ANeg";
+
+        assertEquals(listSuitableDonor.getBloodTypeId(bloodType, id),id);
+    }
+
+    @Test
+    void testPlateletCount(){
+        String id="5d9b8b40-5213-11ec-917b-e2ed2ce588f5";
+        Integer plateletCount=300000;
+
+        assertTrue(listSuitableDonor.getPlateletCount(id,plateletCount));
+    }
+
+    @Test
+    void testHemoglobimCount(){
+        String id="5d9b8b40-5213-11ec-917b-e2ed2ce588f5";
+        Integer hemoglobinCount=120;
+
+        assertTrue(listSuitableDonor.getHemoglobinCount(id,hemoglobinCount));
+    }
+
+    @Test
+    void testRBCCount(){
+        String id="5d9b8b40-5213-11ec-917b-e2ed2ce588f5";
+        Integer rbcCount=300000;
+
+        assertTrue(listSuitableDonor.getRBCCount(id,rbcCount));
     }
 
    
